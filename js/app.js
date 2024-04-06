1a24ff3f63b35e0959c0a501aa2a1aa09c221377
Tyrese <tyrese.k.cardinal@shaw.ca>
2024-04-05 20:22:53 -0600

    Finished with bonus completed as well

---
+++ js/app.js
@@ -0,0 +1,21 @@
+let userAge = 18;
+let is_subscribed = true;
+let user_points = 100;
+
+if (userAge < 18) {
+  console.log("User is not old enough");
+} else {
+  console.log("User is old enough");
+}
+
+if (is_subscribed === true) {
+  console.log("User is subscribed");
+} else {
+  console.log("User is not subscribed");
+}
+
+if (user_points > 99) {
+  console.log("User has 100 or more points")
+} else {
+  console.log("User has less than 100 points")
+}
